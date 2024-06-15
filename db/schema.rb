ce11eba5c99e0e908f1777d92c14f2ceb4e584e7
sumithrasenthil1410 <sumithra.s@rently.com>
2024-06-15 21:25:51 +0530

    initial task controller and table setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_15_144536) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_15_155329) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "status"
+    t.datetime "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
