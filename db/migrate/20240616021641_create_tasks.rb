5ee1c4c2332cb9c986c1dd1bb820f4f734ade045
sumithrasenthil1410 <sumithra.s@rently.com>
2024-06-16 07:50:06 +0530

    migration changes for tasks

--- db/migrate/20240616021641_create_tasks.rb
+++ db/migrate/20240616021641_create_tasks.rb
@@ -1,5 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
+    drop_table :tasks if table_exists?(:tasks)
+    
     create_table :tasks do |t|
       t.string :title
       t.text :description
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_16_020951) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_16_021641) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "due_date"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "due_date"
   end
 
   create_table "users", force: :cascade do |t|
